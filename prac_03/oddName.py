8718a08468c51b58103b9df9e42fc760b20ddd0b
Lochlan Parker <lochlan.parker@my.jcu.edu.au>
2018-03-13 12:05:58 +1000

    Refactor letter counter

--- prac_03/oddName.py
+++ prac_03/oddName.py
@@ -1,6 +1,28 @@
-name = input("What is your name? ")
-while name == '':
-    print('Invalid Input')
+def main():
+    name = get_name()
+    interval = get_interval()
+    extract_letters(name, interval)
+
+
+def get_name():
     name = input("What is your name? ")
-for char in range(1,len(name),2):
-    print(name[char])
+    while name == '':
+        print('Invalid Input')
+        name = get_name()
+    return name
+
+
+def get_interval():
+    interval = int(input("What is the counting interval? "))
+    while interval < 0:
+        print('Invalid Input')
+        interval = get_interval()
+    return interval
+
+
+def extract_letters(name, interval):
+    for char in range(0, len(name), interval):
+        print(name[char])
+
+
+main()
