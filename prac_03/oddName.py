bb871cfe5b9ddc210a5f5e484efd6233b44fc803
Lochlan Parker <lochlan.parker@my.jcu.edu.au>
2018-03-13 12:41:29 +1000

    Refactor printing function

--- prac_03/oddName.py
+++ prac_03/oddName.py
@@ -1,7 +1,7 @@
 def main():
     name = get_name()
     interval = get_interval()
-    extract_letters(name, interval)
+    display_letters(name, interval)
 
 
 def get_name():
@@ -20,7 +20,7 @@ def get_interval():
     return interval
 
 
-def extract_letters(name, interval):
+def display_letters(name, interval):
     for char in range(0, len(name), interval):
         print(name[char])
 
