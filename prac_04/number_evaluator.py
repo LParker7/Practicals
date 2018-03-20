e071de5a3dfe90cb239725eebf3e27cedcbf81ca
jc462898 <lochlan.parker@my.jcu.edu.au>
2018-03-20 11:22:33 +1000

    Add number_evaluator.py

---
+++ prac_04/number_evaluator.py
@@ -0,0 +1,32 @@
+def main():
+    numbers = get_numbers()
+    evaluate_numbers(numbers)
+
+
+def get_numbers():
+    numbers = []
+    for number in range(5):
+        input_number = int(input("Number: "))
+        numbers.append(input_number)
+    return numbers
+
+
+def evaluate_numbers(numbers):
+    print("The first number is {}".format(numbers[0]))
+    print("The last number is {}".format(numbers[-1]))
+    numbers.sort()
+    print("The smallest number is {}".format(numbers[0]))
+    print("The largest number is {}".format(numbers[-1]))
+    average = get_average(numbers)
+    print("The average of the numbers is {}".format(average))
+
+
+def get_average(numbers):
+    sum_of_numbers = 0
+    for number in range(5):
+        sum_of_numbers += numbers[number-1]
+    average = sum_of_numbers / 5
+    return average
+
+
+main()
