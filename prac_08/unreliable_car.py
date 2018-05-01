eb169b3ebbafe0ecba6157daf5929bfdbe505bb8
jc462898 <lochlan.parker@my.jcu.edu.au>
2018-05-01 11:52:46 +1000

    Add SilverServiceTaxi subclass and improve UnreliableCar using .super()

--- prac_08/unreliable_car.py
+++ prac_08/unreliable_car.py
@@ -7,21 +7,13 @@ from random import randint
 
 
 class UnreliableCar(Car):
-
-    def __init__(self, name='Car', fuel='0', reliability='100'):
-        self.name = name
-        self.fuel = fuel
+    def __init__(self, name='Car', fuel=0, reliability=100):
+        super().__init__(name, fuel)
         self.reliability = reliability
-        self.odometer = 0
 
     def drive(self, distance):
-        chance_of_success = randint(0,100)
+        chance_of_success = randint(0, 100)
         if self.reliability > chance_of_success:
-            if distance > self.fuel:
-                distance = self.fuel
-                self.fuel = 0
-            else:
-                self.fuel -= distance
-            self.odometer += distance
-            return distance
-
+            return super().drive(distance)
+        else:
+            return 0
--- prac_08/unreliable_car_test.py
+++ prac_08/unreliable_car_test.py
@@ -1,6 +1,6 @@
 from prac_08.unreliable_car import UnreliableCar
 
 car = UnreliableCar('Unreliable Car', 100, 80)
-for trip in range(0,5):
-    print("Distance travelled in trip {} = {}km".format(trip+1, car.drive(20)))
+for trip in range(0, 5):
+    print("Distance travelled in trip {} = {}km".format(trip + 1, car.drive(20)))
     print(car)
